ccccbbde8b381db9ba5df03692e215cd02e3523a
pattiey <patriciaye99@gmail.com>
2023-03-24 13:27:41 -0700

    remove unused code

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.funcitonal as F
-import torch.distributions
-import torchvision
-import matplotlib.pyplot as plt
-import logging
