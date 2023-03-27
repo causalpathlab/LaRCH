799896f5db7c70ad385ac9c9d16fd5b068751cd1
pattiey <patriciaye99@gmail.com>
2023-03-27 14:32:08 -0700

    starting to remove dependency on np

--- nn/pbt_util.py
+++ nn/pbt_util.py
@@ -1,17 +1,17 @@
 #######################################################
 ## helper functions related to a perfect binary tree ##
 #######################################################
-import numpy as np
+import math
 import torch
 
 #' @param N total number of tree nodes
 #' @return tree depth
 def num_pbt_nodes(kk):
-    tree_depth = np.ceil(np.log2(kk)) + 1
+    tree_depth = math.ceil(math.log2(kk)) + 1
     return (2**tree_depth - 1).astype(int) # number of tree nodes
 
 def pbt_nodes_to_depth(N):
-    return (np.log2(N + 1)).astype(int)
+    return (math.log2(N + 1)).astype(int)
 
 #' @param D tree depth
 def pbt_depth_to_leaves(D):
@@ -21,7 +21,7 @@ def pbt_depth_to_leaves(D):
 #' @param D tree depth
 #' @return S adjacency matrix (#leaves x #tree nodes)
 def pbt_adj(D, _signed = False):
-    if D < 2: out = torch.ones(1)   
+    if D < 2: out = torch.ones(1)
     _N = 2**D - 1
     _nodes = torch.arange(_N)
     _leaves = torch.arange(2**(D-1))
@@ -29,18 +29,18 @@ def pbt_adj(D, _signed = False):
     _col = _bot
     _row = _leaves
     _elem = torch.ones(len(_leaves))
-    
+
     for d in range(1, D):
         _bot = torch.floor((_bot+1)/2)-1
         if _signed:
             _new_elem = 2*torch.ceil(_leaves/2**(d-1)) % 2 - 1
         else:
             _new_elem = torch.ones(len(_leaves))
-        
+
         _elem = torch.cat((_elem, _new_elem))
         _col = torch.cat((_col, _bot)).int()
         _row = torch.cat((_row, _leaves)).int()
-        
+
     out = torch.sparse_coo_tensor(torch.stack([_row, _col],0), _elem)
     return out
 
