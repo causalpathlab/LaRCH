a986fc905bc014f83fbe2d0881c1c1aeeb6f871a
pattiey <patriciaye99@gmail.com>
2023-03-30 17:29:10 -0700

    run main

--- larch/run/sim_real.py
+++ larch/run/sim_real.py
@@ -2,20 +2,24 @@ import os
 import argparse
 from larch.util.simdata_util import sim_real
 
-parser = argparse.ArgumentParser(description='Data simulation parameters')
-parser.add_argument('--N', type=int, help='Number of cells', default=5000)
-parser.add_argument('--noise', type=int, help='Noise parameter', default=50)
-parser.add_argument('--seed', type=int, help='seed', default=123)
-parser.add_argument('--bulk_file', help='filepath to bulk expression file', default='data/mean_tpm_merged.csv')
-parser.add_argument('--out_dir', help='output directory for simulated data', default='data')
+def main():
+    parser = argparse.ArgumentParser(description='Data simulation parameters')
+    parser.add_argument('--N', type=int, help='Number of cells', default=5000)
+    parser.add_argument('--noise', type=int, help='Noise parameter', default=50)
+    parser.add_argument('--seed', type=int, help='seed', default=123)
+    parser.add_argument('--bulk_file', help='filepath to bulk expression file', default='data/mean_tpm_merged.csv')
+    parser.add_argument('--out_dir', help='output directory for simulated data', default='data')
 
-args = parser.parse_args()
-print(args)
+    args = parser.parse_args()
+    print(args)
 
-N = args.N
-noise = args.noise
-seed = args.seed
-bulk_file = args.bulk_file
-outfile = os.path.join(args.out_dir, f"sim_real_N{N}_noise{noise}_seed{seed}.h5ad")
+    N = args.N
+    noise = args.noise
+    seed = args.seed
+    bulk_file = args.bulk_file
+    outfile = os.path.join(args.out_dir, f"sim_real_N{N}_noise{noise}_seed{seed}.h5ad")
 
-sim_real(N, bulk_file, outfile, noise = noise, seed = seed)
+    sim_real(N, bulk_file, outfile, noise = noise, seed = seed)
+
+if __name__ == "__main__":
+    main()
