736eaf26ab264a897ff36c908de4c8cbc24fc231
pattiey <patriciaye99@gmail.com>
2023-04-28 11:26:11 -0700

    depth parameter

--- larch/run/model_inference.py
+++ larch/run/model_inference.py
@@ -11,6 +11,7 @@ from larch.util.util import setup_anndata
 def main():
     parser = argparse.ArgumentParser(description='Inference parameters')
     parser.add_argument('--model_file', help='path to model file', default='model_params.pt')
+    parser.add_argument('--tree_depth', help='Depth of tree of original model', default=5)
     parser.add_argument('--test_data_file', help='path to data file to perform inference on', default='data/sim_data.h5ad')
     parser.add_argument('--out_dir', help='path to save output file', default='data')
 
@@ -21,7 +22,7 @@ def main():
     test_data.layers["counts"] = csr_matrix(test_data.X).copy()
     setup_anndata(test_data, layer="counts")
 
-    model = tree_spike_slab(test_data, 5)
+    model = tree_spike_slab(test_data, args.tree_depth)
     print("model loaded")
     model.load_state_dict(torch.load(args.model_file))
 
@@ -30,6 +31,8 @@ def main():
 
     print("---Saving topic proportions (after softmax)---\n")
     topics_df = pd.DataFrame(test_theta, index= test_data.obs.index, columns = ['topic_' + str(j) for j in range(test_theta.shape[1])])
+    if not os.path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
     topics_df.to_csv(os.path.join(args.out_dir, "topics.csv"))
 
 if __name__ == "__main__":
