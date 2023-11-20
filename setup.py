56fcf9f93865d64b9663cd15d9c0d9a84cc7763a
pattiey <patriciaye99@gmail.com>
2023-11-20 12:00:06 -0800

    remove unused scripts

--- setup.py
+++ setup.py
@@ -10,11 +10,7 @@ setup(
     entry_points={'console_scripts':
         ['spike_slab = larch.run.spike_slab:main',
         'tree_spike_slab = larch.run.tree_spike_slab:main',
-        'tree_susie = larch.run.tree_susie:main',
-        'sc_data_sim = larch.run.sim_real:main',
         'sc_data_sim_rho = larch.run.sim_rho:main',
-        'flat_bayesian = larch.run.flat_bayesian:main',
-        'tree_bayesian = larch.run.tree_bayesian:main',
         'run_inference = larch.run.model_inference:main']
     },
     requires=['pandas', 'torch', 'functools', 'h5py', 'numpy', 'anndata', 'pytorch_lightning', 'pickle', 'scanpy']
