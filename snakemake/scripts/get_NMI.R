d4ed3f418a92fe04d146ca9eafc16b82a5f2e1ae
pattiey <patriciaye99@gmail.com>
2024-03-09 14:07:22 -0800

    include BALSAM in NMI comp

--- snakemake/scripts/get_NMI.R
+++ snakemake/scripts/get_NMI.R
@@ -10,8 +10,9 @@ suppressMessages(library(tidyverse))
 args = commandArgs(trailingOnly = TRUE)
 print(args)
 sc_data_file <- args[1]
-topics_file <- args[2]
-outfile <- args[3]
+tree_topics_file <- args[2]
+BALSAM_topics_file <- args[3]
+outfile <- args[4]
 
 seed <- strsplit(sc_data_file, split = "seed")[[1]][2] 
 seed <- strsplit(seed, split = "[.]")[[1]][1]
@@ -24,19 +25,24 @@ sc_data <- logNormCounts(sc_data, assay.type = "X")
 sc_data <- runPCA(sc_data)
 sc_data <- runUMAP(sc_data)
 
-topics <- fread(topics_file) %>%
+tree_topics <- fread(topics_file) %>%
   remove_rownames() %>% column_to_rownames("V1")
 
-n_topics <- ncol(topics)
+BALSAM_topics <- fread(BALSAM_topics_file) %>% 
+  remove_rownames() %>% column_to_rownames("V1")
+
+n_topics <- ncol(tree_topics)
 topic_factors <- factor(
   paste0('topic_', 0:(n_topics - 1)), 
   levels =  paste0('topic_', 0:(n_topics - 1)))
 
-sc_data$max_topic <- topic_factors[apply(topics, 1, which.max)]
+sc_data$max_topic <- topic_factors[apply(tree_topics, 1, which.max)]
 
 print("Clustering...")
 
-reducedDim(sc_data, "topic_space") <- topics %>% select_if(is.numeric)
+reducedDim(sc_data, "tree_topic_space") <- tree_topics %>% select_if(is.numeric)
+
+reducedDim(sc_data, "BALSAM_topic_space") <- BALSAM_topics %>% select_if(is.numeric)
 
 PCA_clusters <- clusterCells(sc_data, use.dimred = "PCA", BLUSPARAM=NNGraphParam(cluster.fun="louvain"))
 sc_data$PCA_clusters <- PCA_clusters
@@ -44,13 +50,17 @@ sc_data$PCA_clusters <- PCA_clusters
 UMAP_clusters <- clusterCells(sc_data, use.dimred = "UMAP", BLUSPARAM=NNGraphParam(cluster.fun="louvain"))
 sc_data$UMAP_clusters <- UMAP_clusters
 
-z_clusters <- clusterCells(sc_data, use.dimred = "topic_space", BLUSPARAM=NNGraphParam(cluster.fun="louvain"))
-sc_data$z_clusters <- z_clusters
+tree_z_clusters <- clusterCells(sc_data, use.dimred = "tree_topic_space", BLUSPARAM=NNGraphParam(cluster.fun="louvain"))
+sc_data$tree_z_clusters <- tree_z_clusters
+
+BALSAM_z_clusters <- clusterCells(sc_data, use.dimred = "BALSAM_topic_space", BLUSPARAM=NNGraphParam(cluster.fun="louvain"))
+sc_data$BALSAM_z_clusters <- BALSAM_z_clusters
 
 NMI_df <- data.frame(
-  labels = c("max_topic", "LaRCH Cluster", "PCA Cluster", "UMAP Cluster"), 
+  labels = c("max_topic", "LaRCH Cluster", "BALSAM Cluster", "PCA Cluster", "UMAP Cluster"), 
   NMI = c(NMI(sc_data$cell_type, sc_data$max_topic),
-          NMI(sc_data$cell_type, sc_data$z_clusters), 
+          NMI(sc_data$cell_type, sc_data$tree_z_clusters), 
+          NMI(sc_data$cell_type, sc_data$BALSAM_z_clusters), 
           NMI(sc_data$cell_type, sc_data$PCA_clusters),
           NMI(sc_data$cell_type, sc_data$UMAP_clusters)
           )
